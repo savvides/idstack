3767b8ffd5f7280c4dab2dc83d168250507ab3dd
Philippos Savvides <savvides1@gmail.com>
2026-08-15 09:47:23 -0700

    feat(extension): implement DOM extractor for Canvas and web syllabi

---
+++ extension/content/extractor-core.cjs
@@ -0,0 +1,7 @@
+const { detectPageType, extractContentFromDOM, extractPageContent } = require('./extractor.js');
+
+module.exports = {
+  detectPageType,
+  extractContentFromDOM,
+  extractPageContent
+};
